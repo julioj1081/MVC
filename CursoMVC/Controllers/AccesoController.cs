57ad3ffd2370b004171f5a3231f11217cde0d79c
Julio Alberto <julioj1081@hotmail.com>
2020-11-16 16:09:14 -0600

    Clase 1 ajax, form href

---
+++ CursoMVC/Controllers/AccesoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        public ActionResult Enter(string user, string pass)
+        {
+            try
+            {
+
+                return Content("1");
+            }catch(Exception e) { return Content("Ocurrio un error :(" + e.Message); }
+           
+        }
+    }
+}
