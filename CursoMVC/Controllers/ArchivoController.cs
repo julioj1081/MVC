e327862988749085c7fa003df6b6766477b216b2
Julio Alberto <julioj1081@hotmail.com>
2020-11-19 15:39:38 -0600

    Arhivos clonalos y moevelos a una carpeta en especifica usando http

---
+++ CursoMVC/Controllers/ArchivoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CursoMVC.Models.ViewModels;
+
+namespace CursoMVC.Controllers
+{
+    public class ArchivoController : Controller
+    {
+        // GET: Archivo
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(ArchivoViewModel model)
+        {
+            //guarda los 2 archivos en una carpeta en especifica
+            string RutaSitio = Server.MapPath("~/");
+            string PathArchivo1 = Path.Combine(RutaSitio + "/Archivos/archivo1.png");
+            string PathArchivo2 = Path.Combine(RutaSitio + "/Archivos/archivo2.png");
+            if (!ModelState.IsValid)
+            {
+                return View("Index",model);
+            }
+            model.Archivo1.SaveAs(PathArchivo1);
+            model.Archivo1.SaveAs(PathArchivo2);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CursoMVC/Models/ViewModels/ArchivoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models.ViewModels
+{
+    public class ArchivoViewModel
+    {
+        [Required]
+        [Display(Name ="Mi archivo")]
+        public HttpPostedFileBase Archivo1 { get; set; }
+        [Required]
+        [Display(Name = "Mi archivo2")]
+        public HttpPostedFileBase Archivo2 { get; set; }
+        [Required]
+        [Display(Name = "Cadena")]
+        public string Cadena { get; set; }
+    }
+}
